9aff6810fd5d35a44f7c9e5bde1915d6607edd46
Ricky Miller <ricky.miller@gmail.com>
2016-04-19 00:53:55 -0400

    Let there be magic!

---
+++ README.md
@@ -0,0 +1,1 @@
+The Magic 8 Ball is a toy used for fortune-telling or seeking advice.
---
+++ cli.js
@@ -0,0 +1,7 @@
+const arg = process.argv.map((a, i) => i > 1 ? `${a} ` : null).join('')
+const eightball = require('./')()
+
+if(!arg || arg.substring(arg.length - 2) !== '? ')
+  console.log(`ask me a question, silly!`) & process.exit()
+
+console.log(eightball)
